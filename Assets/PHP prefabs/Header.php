a7f454544163cd92ecaaa39cb50b3b408e3ebe1f
smeikol <564500@edu.rocmn.nl>
2023-11-22 11:21:01 +0100

    header fix

--- Assets/PHP prefabs/Header.php
+++ Assets/PHP prefabs/Header.php
@@ -1,19 +1,33 @@
+<?php
+$url = $_SERVER['REQUEST_URI'];
+$parts = parse_url($url);
+$path_parts = explode('/', $parts['path']);
+if ($path_parts[count($path_parts) - 3] == "Admin") {
+    $checker = true;
+} else {
+    $checker = false;
+}
+?>
+
+
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom" style="background-color: 	#089000;">
-  <div class="col-md-3 mb-2 mb-md-0">
-    <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-      <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-    </a>
-  </div>
+    <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+        </a>
+    </div>
 
-  <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-  <li><a href="../../Pages/Home/" class="nav-link px-2" style="color: white;">Home</a></li>
-  <li><a href="../../Pages/Dashboard/" class="nav-link px-2" style="color: white;">Dashboard</a></li>
-</ul>
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="<?php if ($checker) { echo ('../../../Pages/Home/'); } else { echo ('../../Pages/Home/'); } ?>" class="nav-link px-2" style="color: white;">Home</a></li>
+        <li><a href="<?php if ($checker) { echo ('../../../Pages/Dashboard/'); } else { echo ('../../Pages/Dashboard/'); } ?>" class="nav-link px-2" style="color: white;">Dashboard</a></li>
+    </ul>
 
 
-<div class="col-md-3 text-end">
-  <a href="../../Pages/Login/" class="btn btn-outline-light me-2">Login</a>
-  <a href="../../Pages/Signup/" class="btn btn-outline-light me-2">Sign-up</a>
-</div>
+    <div class="col-md-3 text-end">
+        <a href="../../Pages/Login/" class="btn btn-outline-light me-2">Login</a>
+        <a href="../../Pages/Signup/" class="btn btn-outline-light me-2">Sign-up</a>
+    </div>
 
 </header>
--- Pages/Admin/Landingpage/index.php
+++ Pages/Admin/Landingpage/index.php
@@ -1,1 +1,24 @@
-hallo admin
+<?php
+session_start();
+
+if (!(isset($_SESSION['sessionid']) || $_SESSION['sessionid'] == session_id())) {
+    header("location: ../../../Home");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('../../../Assets/css/bootstrap.php'); ?>
+    <?php include('../../../Assets/css/textstyling.php'); ?>
+    <title>Gezondheidsmeter</title>
+</head>
+
+<body>
+    <?php include('../../../Assets/PHP prefabs/Header.php'); ?>
+    hallo admin
+</body>
+
+</html>
