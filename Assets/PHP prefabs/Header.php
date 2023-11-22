59f48e7970a2fbbfa26ce2864e57e8dc525d1297
Dogukan <564937@edu.rocmn.nl>
2023-11-22 09:54:44 +0100

    Header/Bootstrap werkend + Home pagina klaar

--- Assets/PHP prefabs/Header.php
+++ Assets/PHP prefabs/Header.php
@@ -1,12 +1,18 @@
-<div id="header">
-    <nav>
-        <ul>
-            <li><a href="../../Pages/Home/">Home</a></li>
-            <li><a href="../../Pages/Dashboard/">Dashboard</a></li>
-            <li><a href="../../Pages/Vragenlijst/">Vragenlijst</a></li>
-            <li><a href="../../Pages/Login/">Login</a></li>
-            <li><a href="../../Pages/Signup/">Signup</a></li>
-        </ul>
-    </nav>
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom" style="background-color: 	#089000;">
+  <div class="col-md-3 mb-2 mb-md-0">
+    <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+      <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+  </div>
 
-</div>
+  <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+  <li><a href="index.php" class="nav-link px-2" style="color: white;">Home</a></li>
+  <li><a href="#" class="nav-link px-2" style="color: white;">Dashboard</a></li>
+</ul>
+
+
+  <div class="col-md-3 text-end">
+   <a href="../../Pages/Login/" class="btn btn-outline-primary me-2">Login</a>
+   <a href="../../Pages/Signup/" class="btn btn-primary">Sign-up</a>
+  </div>
+</header>
--- Pages/Home/index.php
+++ Pages/Home/index.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gezondheidsmeter</title>
+    <?php include('../../Assets/css/bootstrap.php'); ?> 
+    <?php include('../../Assets/css/textstyling.php'); ?> 
 </head>
 
 <body>
     <?php include ('../../Assets/PHP prefabs/Header.php');?>
-    <div>Welkom bij Overgewicht & Zo, jouw betrouwbare metgezel op weg naar een gezonder leven! Bij Overgewicht & Zo geloven we in de kracht van inzicht en begeleiding op maat om jouw welzijnsdoelen te bereiken. Onze geavanceerde gezondheidsmeter is speciaal ontworpen om je te helpen inzicht te krijgen in jouw gezondheid en welzijn, met een focus op het aanpakken van overgewicht en het bevorderen van een gebalanceerde levensstijl.
+    <div class="centered-text">
+  Welkom bij Overgewicht & Zo, jouw betrouwbare metgezel op weg naar een gezonder leven! Bij Overgewicht & Zo geloven we in de kracht van inzicht en begeleiding op maat om jouw welzijnsdoelen te bereiken. Onze geavanceerde gezondheidsmeter is speciaal ontworpen om je te helpen inzicht te krijgen in jouw gezondheid en welzijn, met een focus op het aanpakken van overgewicht en het bevorderen van een gebalanceerde levensstijl.
 
-        Ontdek de nauwkeurige metingen en gepersonaliseerde inzichten die onze gezondheidsmeter biedt, en laat Overgewicht & Zo jouw partner zijn in het bereiken van jouw gezondheidsdoelen. Volg je vooruitgang, ontvang deskundig advies en neem de regie over je gezondheid in handen.
+</div>
 
-        Bij Overgewicht & Zo begrijpen we dat een gezond gewicht slechts één aspect is van een volledig welzijnsplaatje. Daarom bieden we een holistische benadering die rekening houdt met zowel fysieke als mentale aspecten van gezondheid. Begin vandaag nog aan jouw reis naar een gezonder en evenwichtiger leven met Overgewicht & Zo!
-
-        Jouw welzijn staat voorop bij Overgewicht & Zo. Ontdek de mogelijkheden van onze gezondheidsmeter en laat ons je begeleiden op de weg naar een gezonder, gelukkiger leven. Samen gaan we Overgewicht & Zo te lijf voor een betere versie van jezelf!</div>
 </body>
 
 </html>
