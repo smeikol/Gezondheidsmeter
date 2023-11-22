e136bdfc3e6c83832da6747927d30171d71f6bf8
smeikol <564500@edu.rocmn.nl>
2023-11-22 09:30:45 +0100

    navbar begin

---
+++ Assets/PHP prefabs/Header.php
@@ -0,0 +1,12 @@
+<div id="header">
+    <nav>
+        <ul>
+            <li><a href="../../Pages/Home/">Home</a></li>
+            <li><a href="../../Pages/Dashboard/">Dashboard</a></li>
+            <li><a href="../../Pages/Vragenlijst/">Vragenlijst</a></li>
+            <li><a href="../../Pages/Login/">Login</a></li>
+            <li><a href="../../Pages/Signup/">Signup</a></li>
+        </ul>
+    </nav>
+
+</div>
--- Pages/Home/index.php
+++ Pages/Home/index.php
@@ -1,1 +1,21 @@
-<?php echo "homepage"?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gezondheidsmeter</title>
+</head>
+
+<body>
+    <?php include ('../../Assets/PHP prefabs/Header.php');?>
+    <div>Welkom bij Overgewicht & Zo, jouw betrouwbare metgezel op weg naar een gezonder leven! Bij Overgewicht & Zo geloven we in de kracht van inzicht en begeleiding op maat om jouw welzijnsdoelen te bereiken. Onze geavanceerde gezondheidsmeter is speciaal ontworpen om je te helpen inzicht te krijgen in jouw gezondheid en welzijn, met een focus op het aanpakken van overgewicht en het bevorderen van een gebalanceerde levensstijl.
+
+        Ontdek de nauwkeurige metingen en gepersonaliseerde inzichten die onze gezondheidsmeter biedt, en laat Overgewicht & Zo jouw partner zijn in het bereiken van jouw gezondheidsdoelen. Volg je vooruitgang, ontvang deskundig advies en neem de regie over je gezondheid in handen.
+
+        Bij Overgewicht & Zo begrijpen we dat een gezond gewicht slechts één aspect is van een volledig welzijnsplaatje. Daarom bieden we een holistische benadering die rekening houdt met zowel fysieke als mentale aspecten van gezondheid. Begin vandaag nog aan jouw reis naar een gezonder en evenwichtiger leven met Overgewicht & Zo!
+
+        Jouw welzijn staat voorop bij Overgewicht & Zo. Ontdek de mogelijkheden van onze gezondheidsmeter en laat ons je begeleiden op de weg naar een gezonder, gelukkiger leven. Samen gaan we Overgewicht & Zo te lijf voor een betere versie van jezelf!</div>
+</body>
+
+</html>
