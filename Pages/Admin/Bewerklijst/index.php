072f21da5467453ddf9d49b92ae50dbd40cf5024
Dogukan <564937@edu.rocmn.nl>
2023-12-06 08:53:49 +0100

    Bewerk pagina WIP

--- Pages/Admin/Bewerklijst/index.php
+++ Pages/Admin/Bewerklijst/index.php
@@ -1,1 +1,23 @@
-bewerklijst
+<?php
+session_start();
+
+if (!(isset($_SESSION['sessionid']) || $_SESSION['sessionid'] == session_id()) || $_SESSION['admin'] != "1") {
+    header("location: ../../../Pages/Home");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('../../../Assets/css/bootstrap.php'); ?>
+    <?php include('../../../Assets/css/textstyling.php'); ?>
+    <title>Gezondheidsmeter</title>
+</head>
+
+<body>
+    <?php include('../../../Assets/PHP prefabs/Header.php'); ?>   
+    
+</body>
+</html>
