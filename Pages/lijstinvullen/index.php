c143828d399a6b220c266636390e780b5fc67090
smeikol <564500@edu.rocmn.nl>
2023-12-11 15:19:54 +0100

    vragen

--- Pages/lijstinvullen/index.php
+++ Pages/lijstinvullen/index.php
@@ -1,7 +1,9 @@
 <?php
+include "../../Assets/PHP prefabs/connection.php";
 include('../../Assets/css/bootstrap.php');
 include('../../Assets/css/textstyling.php');
 include('../../Assets/PHP prefabs/Header.php');
+
 
 $today = date("Y-m-d");
 
@@ -11,6 +13,31 @@ $stmt->bind_param("s", $today);
 $stmt->execute();
 $result = $stmt->get_result();
 
+if ($result->num_rows > 0) { 
+
+}
+
+else {
+    $sql2 = "SELECT DISTINCT laatstgebruikt FROM vragen";
+    $stmt2 = $conn->prepare($sql2);
+    $stmt2->execute();
+    $result2 = $stmt2->get_result();
+
+    $alldates = [];
+
+    while ($row = $result2->fetch_array()) {
+        $alldates[] = $row["laatstgebruikt"];
+    }
+    $loopcount = count($alldates);
+    sort($alldates);
+    var_dump($alldates);
+    for ($loopcount; $loopcount > 0; $loopcount--) {
+        $randomdatebias = "vul later in lol ben nog niet klaar";
+    }
+
+
+
+}
 
 
 ?>
