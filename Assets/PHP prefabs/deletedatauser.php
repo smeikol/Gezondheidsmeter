24b87570785409ee1915bb2a5a62e0d5aea51dfd
smeikol <564500@edu.rocmn.nl>
2024-01-19 09:23:51 +0100

    delete data function

---
+++ Assets/PHP prefabs/deletedatauser.php
@@ -0,0 +1,14 @@
+<?php 
+session_start();
+
+if (!(isset($_SESSION['sessionid']) || $_SESSION['sessionid'] == session_id())) {
+    header("location: ../../Pages/Home");
+}
+$userid = $_SESSION['gebruikersid'];
+$sql = "DELETE * FROM `gebruikers_lijst_punten` WHERE gebruikers_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $userid);
+$stmt->execute();
+header("location: ../../Pages/Home");
+
+?>
